dc3670e41407f3cd2073b4d99958dd0dc4776f80
LucaGrbt <52733555+LucaGrbt@users.noreply.github.com>
2022-04-01 10:59:21 +0200

    Add files via upload

---
+++ console_converter_instruction.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 09:45:50 2022
+
+@author: LucaGraebenteich
+
+1) open console (anaconda prompt) of used virtual environment 
+
+2) pyside 2 and/or 6 has to be installed in the used virtual env!
+
+3) first open console of virtual env, then change directory by
+    
+cd /d <absolute path of directory with .ui file>
+
+4) type in:
+
+with PySide2
+
+pyside2-uic <filename.ui> -o <filename.py>
+
+where the filename of the .py file is the wished filename for the resulting converted .py-GUI
+
+5) find the converted .py file in the chosen directory (should be same as .ui file)
+
+
+
+
+"""
