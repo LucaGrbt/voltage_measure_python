f2dbfc3faff893834cdb84b928387af98d1a1b56
LucaGrbt <52733555+LucaGrbt@users.noreply.github.com>
2022-05-09 12:21:33 +0200

    Add files via upload

---
+++ Jenike_Scherzelle_GUI_SpeicherWarnung_Windows.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 18 11:54:45 2022
+
+@author: LucaGraebenteich
+"""
+
+
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class Ui_Save_Error_Window(object):
+    
+    def setupUi(self, Save_Error_Window):
+        Save_Error_Window.setObjectName(u"Save_Warning_Window")
+        Save_Error_Window.resize(270, 50)
+        self.warn_text = QLabel(Save_Error_Window)
+        self.warn_text.setObjectName(u"warn_text")
+        self.warn_text.setGeometry(QRect(5, 5, 260, 40))
+        
+        self.retranslateUi(Save_Error_Window)
+
+        QMetaObject.connectSlotsByName(Save_Error_Window)
+        
+    def retranslateUi(self, Save_Error_Window):
+        Save_Error_Window.setWindowTitle(QCoreApplication.translate("Save_Warning_Window", u"WARNUNG!", None))
+        self.warn_text.setText(QCoreApplication.translate("Save_Warning_Window", u"Gruppenname oder andere Identifikation eingeben\nKeine Slashes ( / ) oder ( : ) verwenden", None))
